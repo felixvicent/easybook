6ad688220cd3e394758e80a8be309215c45ce675
Félix Vicente <felixvicent1306@gmail.com>
2020-04-30 15:58:40 -0300

    Add procfile

--- src/server.js
+++ src/server.js
@@ -1,3 +1,16 @@
 const app = require('./app');
 
-app.listen(3333);
+let port = 0;
+
+switch(process.env.NODE_ENV) {
+    case 'test':
+        port = 3333;
+        break;
+    case 'development':
+        port = 3333;
+        break;
+    default:
+        port = 8080;
+}
+
+app.listen(port);
