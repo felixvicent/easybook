bc3beb56879d6d2e0803abd6e7d2525e19f33780
Félix Vicente <felixvicent1306@gmail.com>
2020-04-30 15:22:00 -0300

    Add procfile

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     development: {
-      username: 'felps',
-      password: 'password',
-      database: 'easybook',
-      host: 'localhost',
+      username: 'yuaheishvegcsx',
+      password: '2d3b77ba618a6ab68fbd917fea5a07ff2e639447e4474df1bfa1cd67b0acf6ae',
+      database: 'd8jd0gse0u9rl8',
+      host: 'ec2-50-17-21-170.compute-1.amazonaws.com',
       dialect: 'mysql',
       logging: false,
       define: {
@@ -11,6 +11,18 @@ module.exports = {
         underscored: true,
       }
     },
+    // development: {
+    //   username: 'felps',
+    //   password: 'password',
+    //   database: 'easybook',
+    //   host: 'localhost',
+    //   dialect: 'mysql',
+    //   logging: false,
+    //   define: {
+    //     timestamps: true,
+    //     underscored: true,
+    //   }
+    // },
     test: {
       username: 'felps',
       password: 'password',
