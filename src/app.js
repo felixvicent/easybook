4c73938165cb327e43d225b0415337044cddd39f
Félix Vicente <felixvicent1306@gmail.com>
2020-04-30 14:55:55 -0300

    Add post manage

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 require('./database/index');
 const auth = require('./auth');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(auth().initialize());
