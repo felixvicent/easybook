3049e9be4d85ad83641c3760d0c06e3a5766e40f
Félix Vicente <felixvicent1306@gmail.com>
2020-05-04 14:09:29 -0300

    Config express static

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const routes = require('./routes');
 
@@ -7,6 +8,7 @@ const auth = require('./auth');
 const app = express();
 
 app.use(cors());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 app.use(express.json());
 app.use(routes);
 app.use(auth().initialize());
